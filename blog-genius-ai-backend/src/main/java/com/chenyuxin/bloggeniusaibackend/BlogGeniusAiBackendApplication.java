32ff0d90dcd822de807c9cc13e420b95a1f30ed0
chenyuxin <2636492078@qq.com>
2025-10-04 15:15:53 +0800

    用户模块

--- blog-genius-ai-backend/src/main/java/com/chenyuxin/bloggeniusaibackend/BlogGeniusAiBackendApplication.java
+++ blog-genius-ai-backend/src/main/java/com/chenyuxin/bloggeniusaibackend/BlogGeniusAiBackendApplication.java
@@ -1,11 +1,13 @@
 package com.chenyuxin.bloggeniusaibackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.chenyuxin.bloggeniusaibackend.mapper")
 public class BlogGeniusAiBackendApplication {
 
     public static void main(String[] args) {
---
+++ blog-genius-ai-backend/src/main/java/com/chenyuxin/bloggeniusaibackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.chenyuxin.bloggeniusaibackend.mapper;
+
+import com.chenyuxin.bloggeniusaibackend.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/chenyuxin0328">chenyuxin</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
